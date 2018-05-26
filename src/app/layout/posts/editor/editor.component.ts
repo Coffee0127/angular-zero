20ddfc2884aeaf737ea2771aee54ec73a292c259
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 16:10:19 +0800

    檢查 Title 是否填寫

--- src/app/layout/posts/editor/editor.component.ts
+++ src/app/layout/posts/editor/editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -10,13 +10,17 @@ export class EditorComponent implements OnInit {
 
   postData = new FormGroup({
     // 透過 FormControl 建構子給定初始值
-    title: new FormControl('Title....'),
+    title: new FormControl('Title....', Validators.required),
     body: new FormControl(),
     tags: new FormArray([
       new FormControl('Angular'),
       new FormControl('HTML')
     ])
   });
+
+  get title(): FormControl {
+    return <FormControl> this.postData.get('title');
+  }
 
   get tags(): FormArray {
     return <FormArray> this.postData.get('tags');
