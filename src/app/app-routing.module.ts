fb203a05a8e93d5f383bc433d1d53877d709e726
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 10:30:41 +0800

    設定路由 - login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './layout/main/main.component';
+import { AuthComponent } from './login/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
 import { PostsModule } from './posts/posts.module';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     PostsModule
   ],
   declarations: [ NavComponent, FooterComponent, MainComponent ],
