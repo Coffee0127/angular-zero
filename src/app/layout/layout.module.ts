6ff51035bef9a3238e6dc20c8a286ecae1065571
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 10:25:22 +0800

    建立 MainComponent

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
 import { PostsModule } from './posts/posts.module';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsModule
   ],
-  declarations: [ NavComponent, FooterComponent ],
+  declarations: [ NavComponent, FooterComponent, MainComponent ],
   exports: [ NavComponent, FooterComponent ]
 })
 export class LayoutModule { }
