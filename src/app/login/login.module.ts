f1b9bad03bc44f72b44b18b0dbe825a6d8bd9533
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 13:25:44 +0800

    加入登入資料物件並雙向繫結

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AuthComponent } from './auth/auth.component';
-import { LoginRoutingModule } from './/login-routing.module';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule
   ],
   declarations: [AuthComponent]
