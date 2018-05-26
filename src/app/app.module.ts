26a0188211303d36ab0e96ac8705ab100130fbb3
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 09:42:10 +0800

    建立 LoginModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
