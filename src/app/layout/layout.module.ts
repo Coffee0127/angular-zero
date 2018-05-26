dd8182a4e45dbe1f1f0cab33729fbb10cf3049d9
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 09:31:57 +0800

    建立 LayoutModule

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ NavComponent, FooterComponent ],
+  exports: [ NavComponent, FooterComponent ]
+})
+export class LayoutModule { }
