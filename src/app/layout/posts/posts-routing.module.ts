4fc49a64616e0a4456705bd39a47827b1da2e31f
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 15:10:24 +0800

    [Fix] 調整 create 路由順序，避免被 id 路由搶走

--- src/app/layout/posts/posts-routing.module.ts
+++ src/app/layout/posts/posts-routing.module.ts
@@ -11,12 +11,12 @@ const routes: Routes = [
     component: PostsComponent
   },
   {
-    path: ':id',
-    component: PostComponent
-  },
-  {
     path: 'create',
     component: EditorComponent
+  },
+  {
+    path: ':id',
+    component: PostComponent
   }
 ];
 
