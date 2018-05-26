7f85e72d9b520868e4470593612235a85cc3e931
Bo-Xuan Fan <coffee0127@gmail.com>
2018-05-26 10:33:40 +0800

    設定路由 - MainComponent 其他路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './layout/main/main.component';
+import { EditorComponent } from './layout/posts/editor/editor.component';
+import { PostComponent } from './layout/posts/post/post.component';
+import { PostsComponent } from './layout/posts/posts/posts.component';
 import { AuthComponent } from './login/auth/auth.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: 'posts/:id',
+        component: PostComponent
+      },
+      {
+        path: 'create',
+        component: EditorComponent
+      }
+    ]
   },
   {
     path: 'login',
